7d036ad0241f84bab040cabe591dba2792869239
xiaoyonggaoya <143922936+xiaoyonggaoya@users.noreply.github.com>
2024-07-01 20:55:31 +0800

    Update script.js

--- filespace/script.js
+++ filespace/script.js
@@ -2,5 +2,5 @@
 console.log("请勿上传违反中华人民共和国相关法律的文件，如检查时遇到将立即删除");
 // 广告推广
 console.log("广告推广：");
-console.log("目前接受广告推广，详情请联系邮箱xiao_yong_gao_ya@163.com");
 console.log("崩坏；星穹铁道 2.3 版本「再见，匹诺康尼」现已开放，更多信息可前往 https://sr.mihoyo.com");
+console.log("目前接受广告推广，详情请联系邮箱xiao_yong_gao_ya@163.com");
