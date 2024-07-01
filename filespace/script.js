8a66f28e6343db0f71c01f551f53b2235e807f44
xiaoyonggaoya <143922936+xiaoyonggaoya@users.noreply.github.com>
2024-07-01 20:42:13 +0800

    Update script.js

--- filespace/script.js
+++ filespace/script.js
@@ -1,1 +1,11 @@
-console.log("text");
+console.log("页面加载开始 0%···");
+console.log("10%···");
+console.log("20%···");
+console.log("30%···");
+console.log("40%···");
+console.log("50%···");
+console.log("60%···");
+console.log("70%···");
+console.log("80%···");
+console.log("90%···");
+console.log("页面加载完成 100%···");
