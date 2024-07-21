ae3f47e98ce2f524e3c1a3c386e6af8626782ebb
xiaoyonggaoya <143922936+xiaoyonggaoya@users.noreply.github.com>
2024-07-21 14:36:58 +0800

    Update script.js

--- filespace/script.js
+++ filespace/script.js
@@ -1,10 +1,2 @@
 // 提示
 console.log("请勿上传违反中华人民共和国相关法律的文件，如检查时遇到将立即删除");
-// 广告推广
-console.log("广告推广：");
-console.log("崩坏；星穹铁道 2.3 版本「再见，匹诺康尼」现已开放，更多信息可前往 https://sr.mihoyo.com — 愿此行，终抵群星。");
-console.log("Minecraft(我的世界)苦力怕论坛 https://klpbbs.com — 最大的我的世界《Minecraft》基岩版（BE）中文资源、交流论坛之一。你可以在这里找到优质的BE版附加包、BE版材质包、BE版地图等资源，以及最新的我的世界新闻资讯，在此与他人交流、分享我的世界游戏经验。各路大神集聚一坛，在这里你可以解决绝大多数问题。");
-console.log("我的世界(Minecraft)ZITBBS小僵尸论坛 https://www.zitbbs.com — 一个萌新论坛,欢迎广大我的世界爱好者们进来交流/学习哦！小站收录了各种我的世界优质资源，你想要的都在这里！你能在这里找到最好玩的整合包,材质包,服务端,光影,插件,模组等");
-console.log("梧州职业学院中职教学部2024秋季学期招生开启，详细信息请见招生简章 https://www.jianpian.cn/a/16roakbz");
-// 广告推广底部
-console.log("目前接受广告推广，详情请联系邮箱xiao_yong_gao_ya@163.com");
